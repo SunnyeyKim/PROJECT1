bdc34c4c7000923b7f69b46909d16e53117cc07a
SunnyeyKim <k0170@nate.com>
2022-06-22 11:09:24 +0900

    step4 수정

--- src/main/java/notice/controller/joinus/LogoutProcController.java
+++ src/main/java/notice/controller/joinus/LogoutProcController.java
@@ -1,5 +1,23 @@
 package notice.controller.joinus;
 
-public class LogoutProcController {
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import notice.controller.Controller;
+import notice.dao.NoticeDao;
+import notice.vo.Notice;
+
+public class LogoutProcController implements Controller{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		System.out.println("LogoutProcController pass");
+
+		request.getSession().invalidate();
+		response.sendRedirect("../customer/notice.do");
+	}
+	
 }
